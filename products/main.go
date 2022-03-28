f2425e37eefd4d5d5a868160824b738ddb38f63b
Eduardo Maturana C <ematurana@wherex.com>
2022-03-27 21:18:39 -0300

    add controller endpoints

--- products/main.go
+++ products/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"ecommerce-ms/products/components"
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
@@ -9,9 +10,11 @@ import (
 func main() {
 	microservice := fiber.New()
 
-	microservice.Get("/api/products", func(c *fiber.Ctx) error {
-		return c.JSON("Hello from products!")
-	})
+	microservice.Get("/api/products", components.GetProductController)
+	microservice.Get("/api/products/:id", components.GetProductController)
+	microservice.Post("/api/products", components.CreateProductController)
+	microservice.Put("/api/products/:id", components.UpdateProductController)
+	microservice.Delete("/api/products/:id", components.DeleteProductController)
 
 	fmt.Println("Server started on port 8080")
 	microservice.Listen(":8080")
